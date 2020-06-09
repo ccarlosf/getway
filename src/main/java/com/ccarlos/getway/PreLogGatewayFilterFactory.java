f9dc01e72aace76ba69694573559e7ee37fe35f8
ccarlosf <2238513100@qq.com>
2020-06-09 18:30:00 +0800

    10-14 进阶：再谈过滤器执行顺序

--- src/main/java/com/ccarlos/getway/PreLogGatewayFilterFactory.java
+++ src/main/java/com/ccarlos/getway/PreLogGatewayFilterFactory.java
@@ -2,6 +2,7 @@ package com.ccarlos.getway;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,8 @@ public class PreLogGatewayFilterFactory
     extends AbstractNameValueGatewayFilterFactory {
     @Override
     public GatewayFilter apply(NameValueConfig config) {
-        return ((exchange, chain) -> {
+
+  /*      return ((exchange, chain) -> {
             log.info("请求进来了...{},{}", config.getName(), config.getValue());
             ServerHttpRequest modifiedRequest = exchange.getRequest()
                 .mutate()
@@ -23,6 +25,19 @@ public class PreLogGatewayFilterFactory
                 .build();
 
             return chain.filter(modifiedExchange);
+        });*/
+
+        GatewayFilter filter = ((exchange, chain) -> {
+            log.info("请求进来了...{},{}", config.getName(), config.getValue());
+            ServerHttpRequest modifiedRequest = exchange.getRequest()
+                    .mutate()
+                    .build();
+            ServerWebExchange modifiedExchange = exchange.mutate()
+                    .request(modifiedRequest)
+                    .build();
+
+            return chain.filter(modifiedExchange);
         });
+        return new OrderedGatewayFilter(filter, 10000);
     }
 }
