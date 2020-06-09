101427d0e7e0d3181a67eb5f7b51cf93af4ca758
ccarlosf <2238513100@qq.com>
2020-06-09 15:06:48 +0800

    Initial commit

---
+++ src/test/java/com/ccarlos/getway/GetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccarlos.getway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GetwayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
